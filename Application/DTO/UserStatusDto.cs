9abe562233a264f98c13cac3123098c115091acd
QuocAnh1502 <anhcnqhe187233@fpt.edu.vn>
2025-03-26 15:41:17 +0700

    ban/unban user

---
+++ Application/DTO/UserStatusDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO
+{
+    public class UserStatusDto
+    {
+        public Guid userId {  get; set; }
+        public bool ban { get; set; }
+    }
+}
